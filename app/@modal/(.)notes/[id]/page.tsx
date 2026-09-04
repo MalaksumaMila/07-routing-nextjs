48b6463f1dff515412d71bcb1953bb5ef239ac73
Mila <malaksumaaa@gmail.com>
2026-09-04 23:20:28 +0200

    update 2

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import NotePreviewClient from '@/components/NotePreviewClient/NotePreview.client';
+import NotePreviewClient from '@/app/@modal/(.)notes/[id]/NotePreviewClient/NotePreview.client';
 
 type Props = {
   params: Promise<{ id: string }>;
